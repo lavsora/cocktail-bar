a8f33d98f6be84740683462a43a32cfffa5ac50e
lavsora <ya.karpich@gmail.com>
2022-11-03 00:11:16 +0300

    update liked function

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import cocktailsReducer from '../features/cocktailsDrink/cocktailsSlice';
-
-export const store = configureStore({
-  reducer: {
-    cocktails: cocktailsReducer,
-  },
-});
